627ec3f09f906c70c744654b331739ab8db0203c
naffins <64361023+naffins@users.noreply.github.com>
2023-04-17 13:47:38 +0800

    Update simple_make_picklable.py

--- simple_make_picklable.py
+++ simple_make_picklable.py
@@ -1,6 +1,9 @@
+import types
+
 def simple_make_picklable(obj):
     """
 Convert an object into a picklable object, by converting everything that is not a basic data type (None, boolean, int, float, complex, str, bytes, bytearrays) or basic data structure (tuple, list, set, dict) into strings.
+Additionally, range and generator objects will be converted to lists.
 
 Parameter(s):
 - obj: Object to be converted
@@ -10,10 +13,10 @@ Returns: Picklable object
 
     if obj is None: return None
     
-    if isinstance(obj,[bool, int, float, complex, str, bytes, bytearray]): return obj
+    if isinstance(obj, (bool, int, float, complex, str, bytes, bytearray)): return obj
 
     if isinstance(obj, tuple):
-        return (simple_make_picklable(i) for i in obj)
+        return tuple((simple_make_picklable(i) for i in obj))
 
     if isinstance(obj, list):
         return [simple_make_picklable(i) for i in obj]
@@ -23,5 +26,11 @@ Returns: Picklable object
     
     if isinstance(obj, dict):
         return {i: simple_make_picklable(obj[i]) for i in obj}
+    
+    if isinstance(obj, range):
+        return list(obj)
+    
+    if isinstance(obj, types.GeneratorType):
+        return [simple_make_picklable(i) for i in list(obj)]
 
     return str(obj)
